972978c146dd9a5c78a6d0370c684624c3879d35
SNicholson99 <scott.nicholson@live.co.uk>
2017-12-16 22:48:34 +0000

    created home page

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import './Navbar.css';
+
+const btn1 = "Home";
+const btn2 = "About Me";
+const btn3 = "Previous Projects";
+const btn4 = "Get In Touch";
+
+class Navbar extends Component {
+  render() {
+    return (
+      <div className="Navbar">
+        <button className="Navbar-button">{btn1}</button>
+        <button className="Navbar-button">{btn2}</button>
+        <button className="Navbar-button">{btn3}</button>
+        <button className="Navbar-button">{btn4}</button>
+      </div>
+    );
+  }
+}
+
+export default Navbar;
