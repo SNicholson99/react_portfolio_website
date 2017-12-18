7fa726fd53bef5a84c07fae945d0c3452497093a
SNicholson99 <scott.nicholson@live.co.uk>
2017-12-18 17:10:27 +0000

    added graphic svg

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import './Home.css';
+import graphic from "./outlineGraphic.svg";
 import profile from "./profile.jpg";
 
 class Home extends Component {
   render() {
     return (
       <div className="Home">
-        <img className="profile" alt="Scott" src={profile} />
-        <div className="header-text">
+        {/* <img className="profile" alt="Scott" src={profile} /> */}
+        <img className="graphic" alt="graphic" src={graphic} />
+        <div className="home-text">
           <h1 className="title">Scott Andrew<br/>Nicholson</h1>
           <h4 className="sh1 sh">Full Stack Developer In Training</h4>
         </div>
