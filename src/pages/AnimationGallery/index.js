e0ea141f958638aa08fdd978dbe58da007c8c53c
SNicholson99 <scott.nicholson@live.co.uk>
2018-01-12 00:06:40 +0000

    fitted toaster to page

--- src/pages/AnimationGallery/index.js
+++ src/pages/AnimationGallery/index.js
@@ -8,7 +8,7 @@ class AnimationGallery extends Component {
   render () {
     return(
         <div className="AnimationGallery">
-          {/* <h2 className="AnimationGallery-title text-heading">Animation Gallery</h2> */}
+          <h2 className="AnimationGallery-title text-heading">Animation Gallery</h2>
           <Toaster />
         </div>
     )
