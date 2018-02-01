ff1a7b04432386b18a9ac433c38b792232219adf
SNicholson99 <scott.nicholson@live.co.uk>
2018-02-01 00:16:55 +0000

    added buzzword animation

--- src/components/ProfileGraphic/index.js
+++ src/components/ProfileGraphic/index.js
@@ -9,6 +9,58 @@ class ProfileGraphic extends Component {
 			<div className="profile-graphic">
 				<div className="graphic-container">
 					<img className="graphic" alt="graphic" src={graphic} />
+					<div className="buzzword-container">
+						<h5 className="buzzword"
+							style={{
+							animationDuration: "5s",
+							position: "absolute",
+							top: "5vh",
+							left: "20vw"
+						}}
+						>
+							JavaScript
+						</h5>
+						<h5 className="buzzword"
+							style={{
+							animationDuration: "5.2s",
+							position: "absolute",
+							top: "16vh",
+							left: "20vw"
+						}}
+						>
+							React
+						</h5>
+						<h5 className="buzzword"
+							style={{
+							animationDuration: "5.5s",
+							position: "absolute",
+							top: "10vh",
+							left: "28vw"
+						}}
+						>
+							HTML
+						</h5>
+						<h5 className="buzzword"
+							style={{
+							animationDuration: "5.8s",
+							position: "absolute",
+							top: "12vh",
+							left: "24vw"
+						}}
+						>
+							CSS
+						</h5>
+						<h5 className="buzzword"
+							style={{
+							animationDuration: "6s",
+							position: "absolute",
+							top: "14vh",
+							left: "26vw"
+						}}
+						>
+							Git
+						</h5>
+					</div>
 				</div>
 			</div>
 		);
